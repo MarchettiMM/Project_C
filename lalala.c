70cff15babe8b7aadc90a2ed2f824759f95a4bbf
Matheus Marchetti Melo <matheusmmarchetti@gmail.com>
2023-03-26 03:11:18 -0300

    Added THI project
    Addition of the project in C that calculates the Temperature and Humidity Index.

---
+++ lalala.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+// Função que calcula o ITU
+float CalculaITU(float temperatura, float umidadeRelativa)
+{
+    return 0.8 * temperatura + umidadeRelativa * ((temperatura - 14.3) / 100) + 46.3;
+}
+
+int CondicaoITU(float ITU)
+{
+    if (ITU < 75)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int main()
+{
+    int Day;
+    int i = 0;
+    struct Dia
+    {
+        int Dias;
+        float Temperatura;
+        float UmidadeRelativa;
+        float ITU;
+        int M;
+    } Dia[365];
+    FILE *TempUmid;
+
+    printf("Seja bem vindo ao programa de verificação de estresse climático!\n\n");
+    TempUmid = fopen("TempUmid.txt", "r");
+    fscanf(TempUmid, "%*s %*s %*s");
+
+    while (fscanf(TempUmid, "%d\t%f\t%f\n", &Dia[i].Dias, &Dia[i].Temperatura, &Dia[i].UmidadeRelativa) != EOF)
+    {
+        Dia[i].ITU = CalculaITU(Dia[i].Temperatura, Dia[i].UmidadeRelativa);
+        Dia[i].M = CondicaoITU(Dia[i].ITU);
+        i++;
+    }
+    fclose(TempUmid);
+    do
+    {
+        do
+        {
+            printf("Digite o dia que deseja-se buscar(Entre 1 e 365)\nCaso queira finalizar o programa, digite 0:\n");
+            scanf("%d", &Day);
+        } while ((Day < 0) || (Day > 365));
+        if (Day != 0)
+        {
+            printf("Temperatura\tUmidade Relativa\tITU\n");
+            printf(" %.2f\t\t %.2f\t\t\t %.2f\t ", Dia[Day - 1].Temperatura, Dia[Day - 1].UmidadeRelativa, Dia[Day - 1].ITU);
+            if (Dia[Day - 1].M == 0)
+            {
+                printf("Condições Normais\n");
+            }
+            else
+            {
+                printf("Estresse Climático\n");
+            }
+        }
+    } while (Day != 0);
+
+    return 0;
+}
